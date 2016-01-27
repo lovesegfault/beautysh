7ea27972eca579fd888df63959544063244580eb
Bernardo Meurer <meurer@MAINFRAME.localdomain>
2016-01-27 00:02:22 -0300

    Began making PEP-257 compliant

--- beautify_bash.py
+++ beautify_bash.py
@@ -1,15 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A Bash script beautifier."""
+# *********************************************************************** #
+#   Copyright (C) 2011, Paul Lutus                                        #
+#                                                                         #
+#   This program is free software; you can redistribute it and/or modify  #
+#   it under the terms of the GNU General Public License as published by  #
+#   the Free Software Foundation; either version 2 of the License, or     #
+#   (at your option) any later version.                                   #
+#                                                                         #
+#   This program is distributed in the hope that it will be useful,       #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#   GNU General Public License for more details.                          #
+#                                                                         #
+#   You should have received a copy of the GNU General Public License     #
+#   along with this program; if not, write to the                         #
+#   Free Software Foundation, Inc.,                                       #
+#   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
+# *********************************************************************** #
+"""A beautifier for Bash scripts."""
 import re
 import sys
 
-PVERSION = '1.0'
+PVERSION = '1.1'
 
 
 class BeautifyBash:
+    """Main class to handle beautifying."""
 
     def __init__(self):
+        """Initializer class """
         self.tab_str = ' '
         self.tab_size = 2
 
