a491465ceffd219b5c6e3dfb587289acefddad3a
Bernardo Meurer <meurerbernardo@gmail.com>
2016-01-31 00:00:48 -0200

    Fixed setup.

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from distutils.core import setup
 
 setup(
     name='beautysh',
-    package=['beautysh'],
-    version='1.0',
+    packages=['beautysh'],
+    version='1.2',
     description='A beautifier for Bash shell scripts written in Python.',
+    license='GPL',
     author='Bernardo Meurer',
     author_email='meurerbernardo@gmail.com',
     url='https://github.com/bemeurer/beautysh',
