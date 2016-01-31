857c9a1082efd935e8f723ffedcb3a52911fe6e7
Bernardo Meurer <meurerbernardo@gmail.com>
2016-01-30 23:42:33 -0200

    Added PyPI support.

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""Setup for beautysh - A bahs beautifier for the masses."""
+from distutils.core import setup
+
+setup(
+    name='beautysh',
+    package=['beautysh'],
+    version='1.0',
+    description='A beautifier for Bash shell scripts written in Python.',
+    author='Bernardo Meurer',
+    author_email='meurerbernardo@gmail.com',
+    url='https://github.com/bemeurer/beautysh',
+    download_url='https://github.com/bemeurer/beautysh/tarball/1.0',
+    keywords=['beautify', 'bash', 'shell', 'beautifier', 'script', 'auto'],
+    classifiers=[]
+)
