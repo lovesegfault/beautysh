a694e3eab1937abd8cefdc0e603399c2dd7484c0
Francesco Montorsi <f18m@users.noreply.github.com>
2019-01-13 10:14:13 +0100

    Fix indent/outdent error for strings containing brackets (#36)

--- tests/test_beautysh.py
+++ tests/test_beautysh.py
@@ -3,6 +3,8 @@ from unittest import TestCase
 import os
 
 TEST_BASIC_FILENAME = os.path.join(os.path.dirname(__file__), 'basictest.sh')
+TEST_INDENT1_RAW_FILENAME = os.path.join(os.path.dirname(__file__), 'indent_test1_raw.sh')
+TEST_INDENT1_BEAUTIFIED_FILENAME = os.path.join(os.path.dirname(__file__), 'indent_test1_beautified.sh')
 
 class TestBasic(TestCase):
     
@@ -16,3 +18,10 @@ class TestBasic(TestCase):
         result, error = Beautify().beautify_string(testdata)
         self.assertFalse(error);  # we expect no parsing error
         self.assertTrue(testdata == result) # we expect no change in formatting
+
+    def test_indent1(self):
+        testdata = self.read_file(TEST_INDENT1_RAW_FILENAME)
+        expecteddata = self.read_file(TEST_INDENT1_BEAUTIFIED_FILENAME)
+        result, error = Beautify().beautify_string(testdata)
+        self.assertFalse(error);  # we expect no parsing error
+        self.assertTrue(expecteddata == result) # we expect no change in formatting
