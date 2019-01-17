69b1a2bcb7860d8715e696fd6b87dcc3c0d71e71
Francesco Montorsi <f18m@users.noreply.github.com>
2019-01-17 20:07:27 +0100

    Function style fixer (#39)
    * Add function style detection and change

---
+++ tests/func_styles_raw.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# this small snippet is to test beautysh function-style detector
+# NOTE: using the string "function" in the function name itself we do a deeper test
+
+function function_style1a()
+{
+	echo "test"
+}
+
+function function_style1b	()
+{
+	echo "test"
+}
+
+function function_style1c ()
+{
+	echo "test"
+}
+
+function function_style1d () {
+	echo "test"
+}
+
+function function_style2a
+{
+	echo "test"
+}
+
+function function_style2b	{
+	echo "test"
+}
+
+function_style3a()
+{
+	echo "test"
+}
+
+function_style3b	()
+{
+	echo "test"
+}
+
+function_style3c ()
+{
+	echo "test"
+}
+
+function_style3d () {
+	echo "test"
+}
+
