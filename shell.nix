1a1169bba8a23266685269695ce8329fa1626eef
Bernardo Meurer <meurerbernardo@gmail.com>
2020-02-18 14:23:30 -0800

    shell: init

---
+++ shell.nix
@@ -0,0 +1,11 @@
+let
+  nixpkgs = import <nixpkgs> { };
+in
+  with nixpkgs;
+  mkShell {
+    name = "beautysh";
+    buildInputs = [
+      bash
+      (python3Full.withPackages (ps: with ps; [ setuptools flake8 nose ]))
+    ];
+}
