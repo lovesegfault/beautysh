90d393dc7f2f3c59cb4c629d547ff5f4df1ca393
Bernardo Meurer <bernardo@meurer.org>
2022-02-04 11:43:09 -0800

    style(flake): call flake-utils utils

--- flake.nix
+++ flake.nix
@@ -3,17 +3,17 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
-    flake-utils.url = "github:numtide/flake-utils";
+    utils.url = "github:numtide/flake-utils";
     poetry2nix = {
       url = "github:nix-community/poetry2nix";
       inputs = {
-        flake-utils.follows = "flake-utils";
+        flake-utils.follows = "utils";
         nixpkgs.follows = "nixpkgs";
       };
     };
   };
 
-  outputs = { nixpkgs, flake-utils, poetry2nix, self }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { nixpkgs, utils, poetry2nix, self }: utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; overlays = [ poetry2nix.overlay ]; };
       inherit (pkgs.lib) attrValues last listToAttrs mapAttrs nameValuePair replaceStrings;
@@ -29,7 +29,7 @@
         program = "${self.packages.${system}.${"beautysh-${pyLatest}"}}/bin/beautysh";
       };
 
-      packages = flake-utils.lib.flattenTree (
+      packages = utils.lib.flattenTree (
         let
           pyVersionToNix = v: "python${replaceStrings ["."] [""] v}";
           pyNixVersions = map pyVersionToNix pyVersions;
